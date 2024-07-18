a4b22ce89b30b9e568a61b8bc4d5b184e90c3a36
Luciana Paiva <143903142+LucianaPaiva@users.noreply.github.com>
2024-07-18 12:18:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+// // Fazer o pix
+
+// let senha = prompt('Digite a senha: ')
+//     if (senha === '123') {
+//         alert('autenticado')
+//     }
+// else {
+//     alert('Falha na autenticação')
+// }
+//---------------------------------------------------------------
+
+// // Pagamento de boleto
+
+// senha = prompt('Digite a senha: ')
+//     if (senha === '123') {
+//         alert('autenticado')
+//     }
+// else {
+//     alert('Falha na autenticação')
+// }
+
+// ------------------------------------------------------------------
+
+// // Recarga de celular
+
+// let senha = prompt('Digite a senha: ')
+//     if (senha === '123') {
+//         alert('autenticado')
+//     }
+// else {
+//     alert('Falha na autenticação')
+// }
+
+// -------------------------------------------------------------------
+
+// function autenticarUsuario() {
+//     let senha = prompt('Digite a senha: ')
+//     let codigo = prompt('Digite o codigo: ')
+//     if (senha === '123' && codigo === '123') {
+//         alert('Autenticado')
+//     }
+//     else{
+//         alert('Falha na autenticação')
+//     }
+// }
+
+// autenticarUsuario()
+
+// Fazer o pix
+
+// ------------------------------------------------------------------------
+
+// function somar(num1, num2) {
+//     let resultado = num1 + num2
+//     return resultado
+// }
+
+// let res = somar(15, 19)
+// alert(res)
+// alert(somar(15, 19))
+
+// --------------------------------------------------------------------------
+//01
+
+// function calcularMedia(n1, n2, n3) {
+//     const media = (n1 + n2 + n3) / 3
+//     return media
+// }
+
+// const resultado = calcularMedia(10, 8.5, 7)
+// alert(resultado)
+
+
+// let nota1 = Number(prompt('Digite a primeira nota: '))
+// let nota2 = Number(prompt('Digite a segunda nota: '))
+// let nota3 = Number(prompt('Digite a terceira nota: '))
+// const res = calcularMedia(nota1, nota2, nota3)
+// alert(res)
+
+// ----------------------------------------------------------------------------
+//02
+
+// function converterTemperatura(celsius) {
+//     const f = (celsius * 9 / 5) + 32
+//     return f
+// }
+
+// const resultado = converterTemperatura(32)
+// alert(resultado)
+
+// -----------------------------------------------------------------------------
+//03
+
+function calcularDistancia(velocidade, tempo) {
+    const distancia = velocidade * tempo
+    return distancia
+}
+
+
+const resultado = calcularDistancia(150, 2)
+alert(resultado)
+
+
+
+
+
